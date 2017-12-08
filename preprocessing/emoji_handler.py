4cabd27a82811e8e49726ddf31b4c3aee68b343f
ian <ian.mackerracher@uni-konstanz.de>
2017-12-08 16:45:06 +0100

    Added emoji lexicon, minor changes to emoji handler

--- preprocessing/emoji_handler.py
+++ preprocessing/emoji_handler.py
@@ -1,71 +1,52 @@
 import os
 import re
 
-def get_emoji_file(file_name):
-    data_path = os.getcwd() + '/'
 
-    with open(data_path + file_name, 'r') as f:
-        raw_text = f.read()
-        raw_text = raw_text.split('\n')
+class EmojiHandler:
 
-    return raw_text
+    def __init__(self, path):
+        #path to emoji lexicon
+        self.path = path
 
+    def get_emoji_file(self):
+        data_path = os.getcwd() + '/'
 
+        with open(data_path + self.path, 'r') as f:
+            raw_text = f.read()
+            raw_text = raw_text.split('\n')
 
-def create_dict(raw_emojis):
-
-    def generate(start, end):
-        start = int(start, 16)
-        end = int(end, 16)
-        unicodes = ['U+'+ re.sub('0x', '', str(hex(i))) for i in range(start, end+1)]
-        return unicodes
+        return raw_text
 
 
 
-    emoji_dict = {}
-    for emoji_line in raw_emojis:
-        try:
-            unicode = re.search(r'^.*?(?=\s)', emoji_line).group()
-            #in case of multiple unicodes
-            unicodes = unicode.split('..')
-            total_unicodes = generate(unicodes[0], unicodes[-1])
-            words = ''.join(re.search(r'(?<=\)\s).*$', emoji_line).group())#.split()
-            words = re.split(r' |\.\.', words)
-            words = [word for word in words if len(word) > 0]
-            for tmp in total_unicodes:
-                emoji_dict[tmp] = words
-        except Exception as e:
-            print(e)
-    return emoji_dict
+    def create_dict(self):
 
 
+        raw_emojis = self.get_emoji_file()
 
-emoji_dict = create_dict(get_emoji_file('emoji_data.txt'))
+        def generate(start, end):
+            start = int(start, 16)
+            end = int(end, 16)
+            unicodes = ['U+'+ re.sub('0x', '', str(hex(i))) for i in range(start, end+1)]
+            return unicodes
+
+        emoji_dict = {}
+        for emoji_line in raw_emojis:
+            try:
+                unicode = re.search(r'^.*?(?=\s)', emoji_line).group()
+                #in case of multiple unicodes
+                unicodes = unicode.split('..')
+                total_unicodes = generate(unicodes[0], unicodes[-1])
+                words = ''.join(re.search(r'(?<=\)\s).*$', emoji_line).group())#.split()
+                words = re.split(r' |\.\.', words)
+                words = [word for word in words if len(word) > 0]
+                for tmp in total_unicodes:
+                    emoji_dict[tmp] = words
+            except Exception as e:
+                print(e)
+        return emoji_dict
 
 
-t = '😍'
-t2 = '❤'
-x = 'U+{:X}'.format(ord(t2))
+emoji_dict = EmojiHandler('emoji_data.txt').create_dict()
 for k in emoji_dict:
     print(k, emoji_dict[k])
-print(emoji_dict[x])
-
-
-
-
-#print(emoji_dict[x])
-
-
-
-
-
-#x = int('0030', 16)
-#y = int('0039', 16)
-#for i in range(x, y+1):
-#    print(hex(i))
-
-
-#emoji_line = e[0]
-#unicode = re.search(r'^.*?(?=\s)', emoji_line).group()
-#words = re.search(r'(?<=\)\s).*$', emoji_line).group().split()
-#print('U+'+unicode, words)
